45692c7605676c2762c261fd7a08a31af818ca3a
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2018-02-20 15:35:26 +0700

    Initial commit

---
+++ lib/WordListsC/Password/SecLists.pm
@@ -0,0 +1,18 @@
+package WordListsC::Password::SecLists;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Lists of popular passwords from SecLists
+
+=head1 DESCRIPTION
+
+This distribution contains the following modules:
+
+# INSERT_MODULES_LIST -/^WordListsC::/
+
+
+=head1 SEE ALSO
+
+=cut
